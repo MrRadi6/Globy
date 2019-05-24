df718c64b8f685bd9a62087bce6fb4691474356c
MrRadix <ahmed.ahmed31193@gmail.com>
2019-05-24 14:07:33 +0200

    Initial Commit

---
+++ SoleekLabTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoleekLabTask
+//
+//  Created by Ahmed Samir on 5/24/19.
+//  Copyright © 2019 MrRadix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
